e7475f6d3e4bdb12a5a98a193cd9924148f4ddb2
Christiano Bolla <christiano.bolla@appwise.be>
2023-04-24 23:23:20 +0200

    Added other calculation options and tests

--- measurements/src/main/java/be/appwise/measurements/Measurement.kt
+++ measurements/src/main/java/be/appwise/measurements/Measurement.kt
@@ -1,53 +1,92 @@
 package be.appwise.measurements
 
 import be.appwise.measurements.units.Dimension
-import be.appwise.measurements.units.UnitMass
+import be.appwise.measurements.units.Unit
 
-class Measurement<in UnitType : Dimension>(var value: Double, unit: UnitType) {
+class Measurement<in UnitType : Unit>(var value: Double, unit: UnitType) {
 
-    companion object
+    companion object {
 
-    var unit: @UnsafeVariance UnitType = unit
-        private set
-
-    fun convert(otherUnit: UnitType) {
-        val newValue = converted(otherUnit)
-        value = newValue.value
-        unit = newValue.unit
-    }
-
-    fun converted(otherUnit: UnitType): Measurement<UnitType> {
-
-        return if (unit == otherUnit) {
-            Measurement(value, otherUnit)
-        } else {
-            val valueInTermsOfBase = unit.converter.baseUnitValue(value)
-            if (otherUnit == otherUnit.baseUnit()) {
-                Measurement(valueInTermsOfBase, otherUnit)
-            } else {
-                val otherValueFromTermsOfBase = otherUnit.converter.value(valueInTermsOfBase)
-                Measurement(otherValueFromTermsOfBase, otherUnit)
-            }
+        fun <UnitType : Dimension> Measurement<UnitType>.convert(otherUnit: UnitType) {
+            val newValue = converted(otherUnit)
+            value = newValue.value
+            unit = newValue.unit
         }
     }
 
-    val description get() = "$value ${unit.symbol}"
+    var unit: @UnsafeVariance UnitType = unit
+        private set
+}
 
-    operator fun <UnitType : Dimension> plus(other: Measurement<UnitType>): Measurement<UnitType> {
-        return if (other.unit.javaClass == this.unit.javaClass) {
-            if (other.unit == unit) {
-                Measurement(value + other.value, unit)
-            } else {
-                val lhsValueInTermsOfBase = unit.converter.baseUnitValue(value)
-                val otherValueInTermsOfBase = other.unit.converter.baseUnitValue(other.value)
-                Measurement(lhsValueInTermsOfBase + otherValueInTermsOfBase, unit.baseUnit())
-            }
+val <UnitType : Dimension> Measurement<UnitType>.description get() = "$value ${unit.symbol}"
+
+fun <UnitType : Dimension> Measurement<UnitType>.converted(otherUnit: UnitType): Measurement<UnitType> {
+    return if (unit == otherUnit) {
+        Measurement(value, otherUnit)
+    } else {
+        val valueInTermsOfBase = unit.converter.baseUnitValue(value)
+        if (otherUnit == otherUnit.baseUnit()) {
+            Measurement(valueInTermsOfBase, otherUnit)
         } else {
-            throw Exception("Attempt to add measurements with non-equal units: ${unit.symbol} and ${other.unit.symbol}")
+            val otherValueFromTermsOfBase = otherUnit.converter.value(valueInTermsOfBase)
+            Measurement(otherValueFromTermsOfBase, otherUnit)
         }
     }
 }
 
+operator fun <UnitType : Dimension> Measurement<UnitType>.plus(other: Measurement<UnitType>): Measurement<UnitType> {
+    return if (other.unit.javaClass == this.unit.javaClass) {
+        if (other.unit == unit) {
+            Measurement(value + other.value, unit)
+        } else {
+            val lhsValueInTermsOfBase = unit.converter.baseUnitValue(value)
+            val otherValueInTermsOfBase = other.unit.converter.baseUnitValue(other.value)
+            Measurement(lhsValueInTermsOfBase + otherValueInTermsOfBase, unit.baseUnit())
+        }
+    } else {
+        throw Exception("Attempt to add measurements with non-equal units: ${unit.symbol} and ${other.unit.symbol}")
+    }
+}
 
+operator fun <UnitType : Dimension> Measurement<UnitType>.minus(other: Measurement<UnitType>): Measurement<UnitType> {
+    return if (other.unit.javaClass == this.unit.javaClass) {
+        if (other.unit == unit) {
+            Measurement(value - other.value, unit)
+        } else {
+            val lhsValueInTermsOfBase = unit.converter.baseUnitValue(value)
+            val otherValueInTermsOfBase = other.unit.converter.baseUnitValue(other.value)
+            Measurement(lhsValueInTermsOfBase - otherValueInTermsOfBase, unit.baseUnit())
+        }
+    } else {
+        throw Exception("Attempt to add measurements with non-equal units: ${unit.symbol} and ${other.unit.symbol}")
+    }
+}
 
+operator fun <UnitType : Dimension> Measurement<UnitType>.div(other: Measurement<UnitType>): Measurement<UnitType> {
+    return if (other.unit.javaClass == this.unit.javaClass) {
+        if (other.unit == unit) {
+            Measurement(value / other.value, unit)
+        } else {
+            val lhsValueInTermsOfBase = unit.converter.baseUnitValue(value)
+            val otherValueInTermsOfBase = other.unit.converter.baseUnitValue(other.value)
+            Measurement(lhsValueInTermsOfBase / otherValueInTermsOfBase, unit.baseUnit())
+        }
+    } else {
+        throw Exception("Attempt to add measurements with non-equal units: ${unit.symbol} and ${other.unit.symbol}")
+    }
+}
+
+operator fun <UnitType : Dimension> Measurement<UnitType>.times(other: Measurement<UnitType>): Measurement<UnitType> {
+    return if (other.unit.javaClass == this.unit.javaClass) {
+        if (other.unit == unit) {
+            Measurement(value * other.value, unit)
+        } else {
+            val lhsValueInTermsOfBase = unit.converter.baseUnitValue(value)
+            val otherValueInTermsOfBase = other.unit.converter.baseUnitValue(other.value)
+            Measurement(lhsValueInTermsOfBase * otherValueInTermsOfBase, unit.baseUnit())
+        }
+    } else {
+        throw Exception("Attempt to add measurements with non-equal units: ${unit.symbol} and ${other.unit.symbol}")
+    }
+}
 
--- measurements/src/test/java/be/appwise/measurements/MeasurementTest.kt
+++ measurements/src/test/java/be/appwise/measurements/MeasurementTest.kt
@@ -1,5 +1,6 @@
 package be.appwise.measurements
 
+import be.appwise.measurements.Measurement.Companion.convert
 import be.appwise.measurements.units.UnitLength
 import be.appwise.measurements.units.UnitMass
 import org.junit.jupiter.api.Assertions.*
@@ -52,6 +53,51 @@ internal class MeasurementTest {
 
         assertThrows(Exception::class.java) {
             m1 + m4
+        }
+    }
+
+    @Test
+    fun testSubtraction() {
+        val m1 = Measurement(1.0, UnitMass.grams)
+        val m2 = Measurement(2.0, UnitMass.grams)
+        val m3 = Measurement(30.0, UnitMass.decigrams)
+        val m4 = Measurement(3.0, UnitLength.kilometers)
+
+        assertEquals(-1.0, (m1 - m2).value)
+        assertEquals(-0.002, (m1 - m3).value)
+
+        assertThrows(Exception::class.java) {
+            m1 - m4
+        }
+    }
+
+    @Test
+    fun testDivision() {
+        val m1 = Measurement(1.0, UnitMass.grams)
+        val m2 = Measurement(2.0, UnitMass.grams)
+        val m3 = Measurement(40.0, UnitMass.decigrams)
+        val m4 = Measurement(3.0, UnitLength.kilometers)
+
+        assertEquals(0.5, (m1 / m2).value)
+        assertEquals(0.25, (m1 / m3).value)
+
+        assertThrows(Exception::class.java) {
+            m1 / m4
+        }
+    }
+
+    @Test
+    fun testMultiplication() {
+        val m1 = Measurement(1.0, UnitMass.grams)
+        val m2 = Measurement(2.0, UnitMass.grams)
+        val m3 = Measurement(40.0, UnitMass.decigrams)
+        val m4 = Measurement(3.0, UnitLength.kilometers)
+
+        assertEquals(2.0, (m1 * m2).value)
+        assertEquals(0.000004, (m1 * m3).value)
+
+        assertThrows(Exception::class.java) {
+            m1 * m4
         }
     }
 }
